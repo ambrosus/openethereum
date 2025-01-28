a1eeedb5dd537327aea4b6f4ae3e461e47f97443
Andrii Rozinko <a.rozinko@gmail.com>
2025-01-28 13:37:31 +0200

    Hardfork at block 34170040

--- crates/ethcore/src/engines/authority_round/mod.rs
+++ crates/ethcore/src/engines/authority_round/mod.rs
@@ -2366,7 +2366,7 @@ impl Engine<EthereumMachine> for AuthorityRound {
                 };
 
                 let signal_number = finalized_header.number();
-                if signal_number == 32323259 {
+                if signal_number == 32323259 || signal_number == 34170040 {
                     info!(target: "engine", "Skipping validator set change signalled at block {}", signal_number);
                     return None;
                 }
